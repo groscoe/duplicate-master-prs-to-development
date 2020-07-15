1ec9219c4a143dcdf63110aeb082631c7f861418
GRoscoe <olwenntaron@gmail.com>
2020-07-15 14:50:21 -0300

    Limit action to PRs to master

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,18 @@ import Webhooks from '@octokit/webhooks';
 export = (app: Application) => {
   app.on('pullrequest.opened', async (context: Context<Webhooks.WebhookPayloadPullRequest>) => {
     const pr = context.payload.pull_request;
-    const repo = context.repo();
 
-    await context.github.pulls.create({
-      owner: repo.owner,
-      repo: repo.repo,
-      title: `[DEV] ${pr.title}`,
-      head: pr.head.label,
-      base: 'development',
-      body: `Ver o [PR pra master](${pr.html_url})`
-    })
+    if (pr.base.ref === 'master') {
+      const repo = context.repo();
+
+      await context.github.pulls.create({
+        owner: repo.owner,
+        repo: repo.repo,
+        title: `[DEV] ${pr.title}`,
+        head: pr.head.ref,
+        base: 'development',
+        body: `Ver o [PR pra master](${pr.html_url})`
+      })
+    }
   })
 }
